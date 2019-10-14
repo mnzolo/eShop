4f92c7c80049458c359acb1588fb4645f4ec4cfd
Mthokozisi <mnzolo@student.wethinkcode.co.za>
2019-10-14 08:48:39 +0200

    all

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <style>
+        <link rel="stylesheet" href="#">
+        .box{
+            border: 1px solid lightblue
+            background-color:lightblue;
+        }
+        h1{
+            font-family:manospace;
+        }
+    </style>
+    <title>eShop</title>
+    </head>
+    <body>
+        <div class="box">
+        <h1>Thanos</h1>
+        </div>
+        <div class="box">
+        <form>
+            <button type="submit" href="registration.php">SingUp</button>
+            <br>
+            <button type="submit" href="sign-in.php">Signin</button>
+        </form>
+        </div>
+    </body>
+</html>
---
+++ registration.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <style>
+        <link rel="stylesheet" href="#">
+        .box{
+            border: 1px solid lightblue
+            background-color:lightblue;
+        }
+        h1{
+            font-family:manospace;
+        }
+    </style>
+    <title>eShop</title>
+    </head>
+    <body>
+        <div class="box">
+        <h1>Thanos</h1>
+        </div>
+        <div class="box">
+        <form>
+            <input type="submit" placeholder="Enter your name">
+            <br>
+            <input type="submit" placeholder="Enter your email">
+            <br>
+            <input type="submit" placeholder="Create Password">
+            <br>
+            <input type="submit" placeholder="Confirm Password">
+            <br>
+            <button type="submit">submit</button>
+        </form>
+        </div>
+    </body>
+</html>
